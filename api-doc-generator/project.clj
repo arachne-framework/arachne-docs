048bad06de0574928535ba34b7aa32a01a6706e8
Luke VanderHart <luke@vanderhart.net>
2017-01-20 11:17:57 -0500

    make namespace list explicit

--- api-doc-generator/project.clj
+++ api-doc-generator/project.clj
@@ -10,7 +10,7 @@
             ;[lein-codox "0.10.2" :exclusions [org.clojure/clojurescript]]
              ]
   :codox {:project {:name "Arachne API Documentation"
-                    :description "Generated API documentation for the Arachne web framework"
+                    :description "Generated API documentation for the Arachne web framework. Please note that this particular build of the documentation is oriented towards Arachne's *users*, and heavily filtered to cut down on noise. Module authors will likely wish to use additional public APIs that are not included in these API docs."
                     :version nil
                     :package nil}
           :source-paths ["../../arachne-core/src"
@@ -20,6 +20,22 @@
                          ;"../../arachne-cljs/src"      ;remove for now, weird CLJS issue
                          ;"../../arachne-figwheel/src"
                          ]
+          :namespaces [arachne.assets.dsl
+                       arachne.build
+                       arachne.core.config.impl.datascript
+                       arachne.core.config.impl.datomic
+                       arachne.core.config.model
+                       arachne.core.dsl
+                       arachne.core.runtime
+                       arachne.error
+                       arachne.http
+                       arachne.http.dsl
+                       arachne.http.config
+                       arachne.http.dsl.test
+                       arachne.log
+                       arachne.pedestal
+                       arachne.pedestal.dsl
+                       arachne.run]
           :html {:namespace-list :flat
                  :transforms [[:head]
                               [:append [:link {:rel "stylesheet"
